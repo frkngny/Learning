7551ff0cf1c694ebc6587c258574df1cfaeabb11
frkngny <frkang35@gmail.com>
2024-02-04 15:48:48 +0300

    Revert "Revert "Initial Commit""
    This reverts commit 073101f388a49901188e119cb0a1b5778ec03e94.

---
+++ Python/django_/dcrm/mydb.py
@@ -0,0 +1,11 @@
+# import psycopg2
+
+# db = psycopg2.connect(host='localhost',
+#                       user='apps_user',
+#                       password='pg_apps1*',
+#                       port='5432',
+#                       database='dcrm')
+
+
+# cursor = db.cursor()
+# cursor.execute('CREATE DATABASE dcrm')
