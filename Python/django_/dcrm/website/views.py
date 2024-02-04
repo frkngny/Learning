a800dc44ac4d3901d046d9b401ded1c68f929727
frkngny <frkang35@gmail.com>
2024-02-04 17:46:08 +0300

    Added user login&logout

--- Python/django_/dcrm/website/views.py
+++ Python/django_/dcrm/website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        pw = request.POST["password"]
+        # Authenticate
+        user = authenticate(request, username=username, password=pw)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in.")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in, please try again...")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
